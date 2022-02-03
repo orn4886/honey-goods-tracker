6a1a981924aae11d726606171875cc031ebc9902
orn4886@gmail.com <orn4886@gmail.com>
2022-02-03 18:33:51 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Button } from 'reactstrap';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button color="primary">꿀매 찾기(거래소)</Button>
+      <Button color="success">꿀매 찾기(경매장)</Button>
     </div>
   );
 }
---
+++ src/pages/auction/Auction.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Auction(){
+
+}
+
+export default Auction;
